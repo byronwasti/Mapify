15abfbb99a43f317a807892c5af9e365e6e06c63
Patrick <patrickhuston1@gmail.com>
2016-03-05 18:58:38 -0500

    Sets up react components

---
+++ react_components/App.jsx
@@ -0,0 +1,24 @@
+var Map = require('./Map.jsx')
+var LoginBox = require('./LoginBox.jsx')
+var Navbar = require('./Navbar.jsx')
+var PlaylistBox = require('./PlaylistBox.jsx')
+var TripPlanner = require('./TripPlanner.jsx')
+
+var App = React.createClass({
+	getInitialState: function(){
+		return {
+
+		}
+	},
+
+	render: function(){
+		return (
+			<h1>Hello world!</h1>
+		)
+	}
+});
+
+ReactDOM.render(
+	<App/>,
+	document.getElementById('content')
+);
---
+++ react_components/Map.jsx
@@ -0,0 +1,4 @@
+
+var Map = React.createClass({
+
+});
