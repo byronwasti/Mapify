6b51bb19a36939365680e6e2f326818ba893dac5
Patrick <patrickhuston1@gmail.com>
2016-03-05 16:48:30 -0500

    Sets up server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var webpackMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var config = './webpack.config.js'
+
+var app = express();
+compiler = webpack(config);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false}));
+
+app.use(express.static(path.join(__dirname, '/public'));  
+app.use(webpackMiddleware(compiler);  
+app.use(webpackHotMiddleware(compiler));
+
+
+app.listen(process.env.PORT || 3000);
+
