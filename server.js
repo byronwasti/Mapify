02f526f22b80907b59612d6ce48ccd25469f2f3d
byronwasti <byron.wasti@gmail.com>
2016-03-07 11:39:19 -0500

    Restructured for production webpack usage

--- server.js
+++ server.js
@@ -5,20 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var webpack = require('webpack');
-var webpackMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
-var config = './webpack.config.js'
 
 var app = express();
-compiler = webpack(config);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false}));
-
-app.use(express.static(path.join(__dirname, '/dist')));  
-app.use(webpackMiddleware(compiler));  
-app.use(webpackHotMiddleware(compiler));
+app.use(express.static(path.join(__dirname, '/public')));  
 
 
 app.listen(process.env.PORT || 3000);
