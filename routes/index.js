d0695355f54a39a8f443c0f914c80b437fb245a9
byronwasti <byron.wasti@gmail.com>
2016-03-08 17:02:22 -0500

    Echonest and spotify authorization setup

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+var rp = require('request-promise');
+var auth = require('../auth');
+
 module.exports = function(){
 	return {
 		verifyLogin: function(req, res){
@@ -12,6 +15,57 @@ module.exports = function(){
 		logout: function(req, res){
 			req.logout();
 			res.redirect('/')
-		}
+		},
+
+        echonest: function(req,res){
+            rp({
+                method: 'GET',
+                uri: 'http://developer.echonest.com/api/v4/song/search',
+                qs: {
+                    api_key: auth.ECHONEST_API_KEY,
+                    format: 'json',
+                    artist: 'radiohead'
+                },
+                json: true
+            })
+            .then(function(songs){
+                res.json(songs);
+            });
+        },
+
+        spotify: function(req, res){
+            rp({
+                method: 'GET',
+                uri: 'https://api.spotify.com/v1/search',
+                qs: {
+                    q: 'radiohead',
+                    type: 'artist'
+                },
+                json: true
+            })
+            .then(function(songs){
+                res.json(songs);
+            });
+        },
+
+        spotify2: function(req, res){
+            console.log(req.session.passport.user.accessToken);
+            rp({
+                method: 'GET',
+                uri: 'https://api.spotify.com/v1/me',
+                qs: {
+                },
+                headers: {
+                    Authorization: req.session.passport.user.accessToken
+                },
+                json: true
+            })
+            .then(function(profile){
+                res.json(profile);
+            })
+            .catch(function(err){
+                res.json(err);
+            });
+        }
 	}
 }
