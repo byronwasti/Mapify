d4bb9966414bee4db48aac14c5b4bd137f1af1c0
keenanzucker <keenan.zucker@students.olin.edu>
2016-03-08 17:06:08 -0500

    flexbox styling is working allright

---
+++ react_components/PlaylistBox.jsx
@@ -0,0 +1,13 @@
+var React = require("react");
+
+var PlaylistBox = React.createClass({
+	render: function(){
+		return (
+			<div className="playlist-container">
+				<h1>I am a playlistbox!</h1>
+			</div>
+		)
+	}
+});
+
+module.exports = PlaylistBox;
