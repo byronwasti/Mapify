1b0b0fe81ff05b633e3421ce167e81237515219c
keenanzucker <keenan.zucker@students.olin.edu>
2016-03-08 22:27:51 -0500

    added framework for playlist

--- react_components/PlaylistBox.jsx
+++ react_components/PlaylistBox.jsx
@@ -4,10 +4,42 @@ var PlaylistBox = React.createClass({
 	render: function(){
 		return (
 			<div className="playlist-container">
-				<h1>I am a playlistbox!</h1>
+				<h1>Your Custom Playlist</h1>
+				<SongList/>
+				<AddToSpotify />
 			</div>
 		)
 	}
 });
+
+var SongList = React.createClass({
+
+	render: function(){
+		return(
+		<div className="song-list">
+			<ul>
+				<li>Song 1 </li>
+				<li>Song 2</li>
+				<li>Song 3</li>
+			</ul>
+		</div>
+		)
+	}
+});
+
+var AddToSpotify = React.createClass({
+
+	render: function(){
+		return (
+
+			<div className="add-to-spotify">
+				<input type="button" value="ADD TO SPOTIFY" />
+			</div>
+		)
+	}
+});
+
+
+
 
 module.exports = PlaylistBox;
