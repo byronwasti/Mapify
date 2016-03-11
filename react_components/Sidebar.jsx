867aabb1ed8708039625e55298c7e9e6fdfc5bd0
Patrick <patrickhuston1@gmail.com>
2016-03-11 00:48:36 -0500

    Sidebar highlighting functionality complete

--- react_components/Sidebar.jsx
+++ react_components/Sidebar.jsx
@@ -7,11 +7,34 @@ var CONTENTMAP = 'map',
 
 var Sidebar = React.createClass({
 	render: function(){
+
+		var mapStyle = {};
+		var musicStyle = {};
+		var playlistStyle = {};
+
+		var highlightStyle = {
+		  background: 'black',
+		};
+
+		switch(this.props.contentStatus){
+			case CONTENTMAP:
+				mapStyle = highlightStyle;
+				break;
+
+			case CONTENTMUSIC:
+				musicStyle = highlightStyle;
+				break;
+
+			case CONTENTPLAYLIST:
+				playlistStyle = highlightStyle;
+				break;
+		}
+
 		return (
 			<div className="sidebar-container">
-				<h2 className="header-1"><span><i className="material-icons">navigation</i></span>Map</h2>
-				<h2 className="header-2"><span><i className="material-icons">music_note</i></span>Music</h2>
-				<h2 className="header-3"><span><i className="material-icons">playlist_play</i></span>Playlist</h2>
+				<div style={mapStyle}><h2 className="header-1"><span><i className="material-icons">navigation</i></span>Map</h2></div>
+				<div style={musicStyle}><h2 className="header-2"><span><i className="material-icons">music_note</i></span>Music</h2></div>
+				<div style={playlistStyle}><h2 className="header-3"><span><i className="material-icons">playlist_play</i></span>Playlist</h2></div>
 			</div>
 		)
 	}
