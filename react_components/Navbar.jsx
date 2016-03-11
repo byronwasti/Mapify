f6e42429f716d59d38bf2349e2240e4f1f8cec08
keenanzucker <keenan.zucker@students.olin.edu>
2016-03-11 01:29:47 -0500

    added sweet logo

--- react_components/Navbar.jsx
+++ react_components/Navbar.jsx
@@ -5,6 +5,7 @@ var Navbar = React.createClass({
 	render: function(){
 		return (
 			<div className='navbar-container'>
+				<img className="logo-nav" src="/images/logo.png" />
 				<h2 className='navbar-name'>{this.props.username}
 				<span><a className="logout-button" href="/logout">LOGOUT</a></span></h2>
 				
