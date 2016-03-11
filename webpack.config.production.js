8c08d1ecf8cf684d4591d29f585613b968170d19
byronwasti <byron.wasti@gmail.com>
2016-03-11 02:00:13 -0500

    Webpack configuration for proudction server

---
+++ webpack.config.production.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+var precss       = require('precss');
+
+module.exports = {
+    entry: './react_components/App.jsx',
+    output: {
+        path: path.join(__dirname, '/public/'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+        {
+            //tell webpack to use jsx-loader for all *.jsx files
+            test: [/\.js$/,/\.jsx$/],
+            exclude: '/node_modules/',
+            loader: 'babel-loader',
+            query: {
+                cacheDirectory: true,
+                presets: ['react', 'es2015']
+            }
+        },
+        {
+            test: require.resolve("react"), 
+            loader: "expose?React" 
+        },
+        {
+            test:   [/\.css$/, /\.scss$/],
+            //loader: ['style-loader','css-loader','postcss-loader', 'sass']
+            loaders: ['style','css','postcss-loader', 'sass']
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.scss', '.css']
+    },
+    postcss: function () {
+        return [autoprefixer, precss];
+    },
+};
