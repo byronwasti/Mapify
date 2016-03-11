0a6275434927d8cc3f19f8372b69c097f40f0818
keenanzucker <keenan.zucker@students.olin.edu>
2016-03-10 21:35:34 -0500

    song list

--- react_components/PlaylistBox.jsx
+++ react_components/PlaylistBox.jsx
@@ -6,19 +6,44 @@ var PlaylistBox = React.createClass({
 		return (
 			<div className="playlist-container">
 				<h1 className="playlist-header">Your Custom Playlist:</h1>
+				<SongList
+				songList={this.props.songList}/>
 			</div>
 		)
 	}
 });
 
+var SongList = React.createClass({
+	render: function(){
+		var songNodes = this.props.songList.map(function(song, index){
+			if (song){
+				return (
+					<Song 
+						key={index}
+						song={song} />
+				);
+			}
+		}, this);
+
+		return (
+			<div className='song-list'>
+				{songNodes}
+			</div>
+		)
+	}
+
+});
+
 var Song = React.createClass({
     render: function(){
+    	console.log("Props: ", this.props);
         return (
-                <div>
-    <p> {this.props.song.title} <br/>
-     --{this.props.song.artist_name} </p>
-    </div>
-               );
+            <div className="song">
+            	<input type="button" value="Play"/>
+            	<p>{this.props.song.title}</p>
+            	<p>{this.props.song.artist_name}</p>
+    		</div>
+        );
     }
 });
 
