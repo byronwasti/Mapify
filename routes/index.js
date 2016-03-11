1eaecdaa0323c043de38e32cbff0ccffc6b24b2a
byronwasti <byron.wasti@gmail.com>
2016-03-10 21:38:40 -0500

    Added spotify sound route

--- routes/index.js
+++ routes/index.js
@@ -24,60 +24,24 @@ module.exports = function(){
             
         },
 
-        echonest: function(req,res){
-            rp({
-                method: 'GET',
-                uri: 'http://developer.echonest.com/api/v4/song/search',
-                qs: {
-                    api_key: auth.ECHONEST_API_KEY,
-                    format: 'json',
-                    artist: 'radiohead'
-                },
-                json: true
-            })
-            .then(function(songs){
-                res.json(songs);
-            });
-        },
-
-        spotify: function(req, res){
-            rp({
-                method: 'GET',
-                uri: 'https://api.spotify.com/v1/search',
-                qs: {
-                    q: 'radiohead',
-                    type: 'artist'
-                },
-                json: true
-            })
-            .then(function(songs){
-                res.json(songs);
-            });
-        },
-
-        spotify2: function(req, res){
-            console.log(req.session.passport.user.accessToken);
-            rp({
-                method: 'GET',
-                uri: 'https://api.spotify.com/v1/me',
-                qs: {
-                },
-                headers: {
-                    Authorization: 'Bearer ' + req.session.passport.user.accessToken
-                },
-                json: true
-            })
-            .then(function(profile){
-                res.json(profile);
-            })
-            .catch(function(err){
-                res.json(err);
-            });
-        },
-
         lookupMusic: function(req, res){
             echonest_search(req, res);
-        }
+        },
 
+        thirtySecondSample: function(req, res){
+            rp({
+                method: 'GET',
+                uri: 'https://api.spotify.com/v1/tracks/'+req.query.id,
+                json: true
+            })
+            .then(function(result){
+                res.json(result);
+            })
+            .catch(function(err){
+                console.error(err);
+                res.json(err);
+            });
+
+        }
 	}
 }
