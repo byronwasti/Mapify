61be03630d155e4789b8986b7a801340a0744010
Patrick <patrickhuston1@gmail.com>
2016-03-11 01:21:20 -0500

    Cleaning up

--- react_components/App.jsx
+++ react_components/App.jsx
@@ -37,8 +37,7 @@ var App = React.createClass({
 					this.setState({
 						user: data.user,
 						loggedIn: true,
-						//content: CONTENTMAP
-						content: CONTENTMUSIC
+						content: CONTENTMAP
 					});
 				} else {
 					this.setState({
